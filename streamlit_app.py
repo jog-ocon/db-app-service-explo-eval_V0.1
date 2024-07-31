c56b495d4fe36570600f6ec89f688b5598a28cff
jog-ocon <162304709+jog-ocon@users.noreply.github.com>
2024-08-01 01:25:40 +0200

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ from app.projet_comparaison.upload_excel import upload_explo, upload_eval
 from utility_functions import run_script, sort_alfanumeric
 from app.sidebar_structure import test_choice
 #analysis
-from analysis.data_cleaning import clean_join_df, join_dataframes
-from analysis.transform import group_and_calculate_percentage, group_and_calculate_percentage_with_names, join_and_rename_columns, group_and_get_filtered_max_value, group_and_get_filtered_min_value, join_dataframes_jaro,filter_dataframes_jaro, get_combined_total_ges, sum_top_n_elements, find_sref_batiment_and_next
+from analysis.data_cleaning import clean_join_df, join_dataframes, group_and_aggregate
+from analysis.transform import group_and_calculate_percentage, group_and_calculate_percentage_with_names, join_and_rename_columns, group_and_get_filtered_max_value, group_and_get_filtered_min_value, join_dataframes_jaro,filter_dataframes_jaro, get_combined_total_ges, sum_top_n_elements, find_sref_batiment_and_next, change_column_if_not_in_list
 from analysis.data_viz import transform_and_plot_stacked_bar, transform_and_plot_stacked_bar_lot_et_slot, plot_butterfly_chart, plot_bar_chart_h
 # run_script("sidebar/sidebar_structure.py")
 
@@ -75,7 +75,8 @@ elif choice == "Projets comparaison":
                 #Making the stacked barplot from the choices
                 transform_and_plot_stacked_bar(filtered_lots_eval_explo, 'Lot', ['eval', 'explo'])
 
-            jaro_join_df = join_dataframes_jaro(df_eval, df_explo)
+            jaro_join_df = group_and_aggregate(join_dataframes_jaro(df_eval, df_explo))
+
 
             #set number of top fiches
             with st.container(border=True) :
@@ -96,7 +97,7 @@ elif choice == "Projets comparaison":
 
             #BUTTERFLY CHART FOR TOP FICHES
               
-            top_fiches_alike = plot_butterfly_chart(jaro_join_df, top_n=selected_option_top_fiches_)
+            # top_fiches_alike = plot_butterfly_chart(jaro_join_df, top_n=selected_option_top_fiches_)
             info_checkbox_ = st.checkbox("💡 Table with the components in EVAL AND EXPLO")
             if info_checkbox_:
                 st.write(jaro_join_df)
@@ -128,79 +129,144 @@ elif choice == "Projets comparaison":
                 # Create the select box with the initial option set by value
                 selected_option = st.selectbox("Select a Lot", select_options, index=select_options.tolist().index(initial_option))
                 
+                #
+                sous_lot_eval = group_and_calculate_percentage(df_eval, 'Sous-lot', 'GES_divided_by__SREF', 'Lot')
+                sous_lot_explo = group_and_calculate_percentage(df_explo, 'Sous-lot', 'GES_divided_by__SREF', 'Lot')
+                sous_lots_eval_explo = join_and_rename_columns(sous_lot_eval, sous_lot_explo, 'Sous-lot')
+                              
+                #
+
                 # Convert the selected option to a list
                 selected_option_list = [selected_option]
                 #getting max values of lots for the y axis scale
-                max_eval = group_and_get_filtered_max_value(df_eval, 'Lot', 'GES_divided_by__SREF', selected_option)
-                max_explo = group_and_get_filtered_max_value(df_explo, 'Lot', 'GES_divided_by__SREF', selected_option)
-                y_axis_lots = round(max(max_eval, max_explo)*1.2,0)
+            
 
                 filtered_exploration_eval = df_eval[df_eval['Lot'].isin(selected_option_list)]
                 filtered_exploration_explo = df_explo[df_explo['Lot'].isin(selected_option_list)]
 
-                col1, col2 = st.columns(2)
+                #
 
-                with col1:
-                    st.write("EVAL exploration")
-
-                    exploration_lots_eval = group_and_calculate_percentage_with_names(filtered_exploration_eval, 'Sous-lot', 'GES_divided_by__SREF')
-                    
-                    #Making the stacked barplot from the choices
-                    filtered_exploration_lots_eval = group_and_calculate_percentage(exploration_lots_eval, 'Sous-lot', 'GES_divided_by__SREF')
-                    transform_and_plot_stacked_bar_lot_et_slot(filtered_exploration_lots_eval, 'Sous-lot', ['GES_divided_by__SREF'], [0, y_axis_lots])
+                #
+                    #
                 
-                with col2:
-                    st.write("EXPLO exploration")
-                    exploration_lots_explo = group_and_calculate_percentage_with_names(filtered_exploration_explo, 'Sous-lot', 'GES_divided_by__SREF')
+
+                filtered_sous_lots_eval_explo = sous_lots_eval_explo[sous_lots_eval_explo['Lot_x'].isin(selected_option_list)]
+
+
+                #Making the stacked barplot from the choices
+                transform_and_plot_stacked_bar(filtered_sous_lots_eval_explo, 'Sous-lot', ['eval', 'explo'], color_palette='Vivid')
+                
+                # test = lots_eval_explo[~lots_eval_explo['Sous-lot'].isin(selected_options)]
+                # st.write
+                #
+                #
+
+                #
+
+                # col1, col2 = st.columns(2)
+
+                # with col1:
+                #     st.write("EVAL exploration")
+
+                #     exploration_lots_eval = group_and_calculate_percentage_with_names(filtered_exploration_eval, 'Sous-lot', 'GES_divided_by__SREF')
                     
-                    #Making the stacked barplot from the choices
-                    filtered_exploration_lots_explo = group_and_calculate_percentage(exploration_lots_explo, 'Sous-lot', 'GES_divided_by__SREF')
-                    transform_and_plot_stacked_bar_lot_et_slot(filtered_exploration_lots_explo, 'Sous-lot', ['GES_divided_by__SREF'], [0, y_axis_lots])
+                #     #Making the stacked barplot from the choices
+                #     filtered_exploration_lots_eval = group_and_calculate_percentage(exploration_lots_eval, 'Sous-lot', 'GES_divided_by__SREF')
+                #     transform_and_plot_stacked_bar_lot_et_slot(filtered_exploration_lots_eval, 'Sous-lot', ['GES_divided_by__SREF'], [0, y_axis_lots])
+
+
+                
+
+
+
+                # with col2:
+                #     st.write("EXPLO exploration")
+                #     exploration_lots_explo = group_and_calculate_percentage_with_names(filtered_exploration_explo, 'Sous-lot', 'GES_divided_by__SREF')
+                    
+                #     #Making the stacked barplot from the choices
+                #     filtered_exploration_lots_explo = group_and_calculate_percentage(exploration_lots_explo, 'Sous-lot', 'GES_divided_by__SREF')
+                #     transform_and_plot_stacked_bar_lot_et_slot(filtered_exploration_lots_explo, 'Sous-lot', ['GES_divided_by__SREF'], [0, y_axis_lots])
                 
                 #LOT STACKED BARS ------------------------- Hacer esto un py file o muchas funciones para reutilizar en sous-lots
                 #creation of select for lot
-                sous_lot_select_options = (filtered_exploration_eval['Sous-lot'].unique())
+                composants_select_options = (filtered_sous_lots_eval_explo['Sous-lot'].unique())
                 # Set the initial option as a string value
-                initial_option = sous_lot_select_options[0]
+                initial_option = composants_select_options[0]
                 # Create the select box with the initial option set by value
-                sous_lot_selected_option = st.selectbox("Select a Sous-lot", sous_lot_select_options, index=sous_lot_select_options.tolist().index(initial_option))
+                composants_selected_option = st.selectbox("Select a Sous-lot", composants_select_options, index=composants_select_options.tolist().index(initial_option))
 
                 # Convert the selected option to a list
-                sous_lot_selected_option_list = [sous_lot_selected_option]
+                composants_selected_option_list = [composants_selected_option]
+
+
+                #
+                # group_and_calculate_percentage_with_names(df, group_by_col, sum_col, names_col1='Donnée environnementale (DE)', names_col2='Lot')
+                #
+                composants_eval = group_and_calculate_percentage_with_names(df_eval, 'Donnée environnementale (DE)', 'GES_divided_by__SREF', 'Sous-lot')
+                composants_explo = group_and_calculate_percentage_with_names(df_explo, 'Donnée environnementale (DE)', 'GES_divided_by__SREF', 'Sous-lot')
+                composants_eval_explo = join_and_rename_columns(composants_eval, composants_explo, 'Donnée environnementale (DE)')
+                              
+                #
+
+                #
+
+                #
+                    #
+                
+
+                # filtered_composants_eval_explo = composants_eval_explo[composants_eval_explo['Sous-lot_x'].isin(selected_option_list)]
+                filtered_composants_eval_explo = composants_eval_explo[
+                    (composants_eval_explo['Sous-lot_x'].isin(composants_selected_option_list)) |
+                    (composants_eval_explo['Sous-lot_y'].isin(composants_selected_option_list))
+                ]
+
+                #Making the stacked barplot from the choices
+                filtered_composants_eval_explo = change_column_if_not_in_list(filtered_composants_eval_explo, 'Sous-lot_x', composants_selected_option_list, 'eval')
+                filtered_composants_eval_explo = change_column_if_not_in_list(filtered_composants_eval_explo, 'Sous-lot_y', composants_selected_option_list, 'explo')
+
+                # st.write(filtered_composants_eval_explo)
+                # st.write(test2)
+                transform_and_plot_stacked_bar(filtered_composants_eval_explo, 'Donnée environnementale (DE)', ['eval', 'explo'], color_palette='Vivid')
+                
+
+                #
+
+
+
                 #getting max values of lots for the y axis scale 
 
-                sous_lot_max_eval = group_and_get_filtered_max_value(df_eval, 'Sous-lot', 'GES_divided_by__SREF', sous_lot_selected_option)
-                sous_lot_max_explo = group_and_get_filtered_max_value(df_explo, 'Sous-lot', 'GES_divided_by__SREF', sous_lot_selected_option)
-                y_axis_sous_lots = round(max(sous_lot_max_eval, sous_lot_max_explo)*1.2,0)
+                # sous_lot_max_eval = group_and_get_filtered_max_value(df_eval, 'Sous-lot', 'GES_divided_by__SREF', sous_lot_selected_option)
+                # sous_lot_max_explo = group_and_get_filtered_max_value(df_explo, 'Sous-lot', 'GES_divided_by__SREF', sous_lot_selected_option)
+                # y_axis_sous_lots = round(max(sous_lot_max_eval, sous_lot_max_explo)*1.2,0)
 
-                #getting min values
-                # st.write(sous_lot_selected_option)
-                sous_lot_min_eval = group_and_get_filtered_min_value(df_eval, 'Sous-lot','Donnée environnementale (DE)', 'GES_divided_by__SREF', sous_lot_selected_option)
-                sous_lot_min_explo = group_and_get_filtered_min_value(df_explo, 'Sous-lot', 'Donnée environnementale (DE)','GES_divided_by__SREF', sous_lot_selected_option)
-                y_axis_min_sous_lots = round(min(sous_lot_min_eval, sous_lot_min_explo)*1.1,0)
+                # #getting min values
+                # # st.write(sous_lot_selected_option)
+                # sous_lot_min_eval = group_and_get_filtered_min_value(df_eval, 'Sous-lot','Donnée environnementale (DE)', 'GES_divided_by__SREF', sous_lot_selected_option)
+                # sous_lot_min_explo = group_and_get_filtered_min_value(df_explo, 'Sous-lot', 'Donnée environnementale (DE)','GES_divided_by__SREF', sous_lot_selected_option)
+                # y_axis_min_sous_lots = round(min(sous_lot_min_eval, sous_lot_min_explo)*1.1,0)
 
-                sous_lot_filtered_exploration_eval = filtered_exploration_eval[filtered_exploration_eval['Sous-lot'].isin(sous_lot_selected_option_list)]
-                sous_lot_filtered_exploration_explo = filtered_exploration_explo[filtered_exploration_explo['Sous-lot'].isin(sous_lot_selected_option_list)]
+                # sous_lot_filtered_exploration_eval = filtered_exploration_eval[filtered_exploration_eval['Sous-lot'].isin(sous_lot_selected_option_list)]
+                # sous_lot_filtered_exploration_explo = filtered_exploration_explo[filtered_exploration_explo['Sous-lot'].isin(sous_lot_selected_option_list)]
 
-                col1, col2 = st.columns(2)
+                # col1, col2 = st.columns(2)
 
-                with col1:
-                    st.write("EVAL sous-lot exploration")
-                    exploration_sous_lots_eval = group_and_calculate_percentage_with_names(sous_lot_filtered_exploration_eval, 'Sous-lot', 'GES_divided_by__SREF')
+                # with col1:
+                #     st.write("EVAL sous-lot exploration")
+                #     exploration_sous_lots_eval = group_and_calculate_percentage_with_names(sous_lot_filtered_exploration_eval, 'Sous-lot', 'GES_divided_by__SREF')
                     
-                    #Making the stacked barplot from the choices
-                    sous_lot_filtered_exploration_eval = group_and_calculate_percentage(sous_lot_filtered_exploration_eval, 'Donnée environnementale (DE)', 'GES_divided_by__SREF')
-                    max_value = round(sous_lot_filtered_exploration_eval.GES_divided_by__SREF.sum(),2)
-                    transform_and_plot_stacked_bar_lot_et_slot(sous_lot_filtered_exploration_eval, 'Donnée environnementale (DE)', ['GES_divided_by__SREF'], [y_axis_min_sous_lots, y_axis_sous_lots])
+                #     #Making the stacked barplot from the choices
+                #     sous_lot_filtered_exploration_eval = group_and_calculate_percentage(sous_lot_filtered_exploration_eval, 'Donnée environnementale (DE)', 'GES_divided_by__SREF')
+                #     max_value = round(sous_lot_filtered_exploration_eval.GES_divided_by__SREF.sum(),2)
+                #     transform_and_plot_stacked_bar_lot_et_slot(sous_lot_filtered_exploration_eval, 'Donnée environnementale (DE)', ['GES_divided_by__SREF'], [y_axis_min_sous_lots, y_axis_sous_lots])
                 
-                with col2:
-                    st.write("EXPLO sous-lot  exploration")
-                    exploration_sous_lots_explo = group_and_calculate_percentage_with_names(sous_lot_filtered_exploration_explo, 'Sous-lot', 'GES_divided_by__SREF')
+                # with col2:
+                #     st.write("EXPLO sous-lot  exploration")
+                #     exploration_sous_lots_explo = group_and_calculate_percentage_with_names(sous_lot_filtered_exploration_explo, 'Sous-lot', 'GES_divided_by__SREF')
                     
-                    #Making the stacked barplot from the choices
-                    sous_lot_filtered_exploration_explo = group_and_calculate_percentage(sous_lot_filtered_exploration_explo, 'Donnée environnementale (DE)', 'GES_divided_by__SREF')
-                    max_value = round(sous_lot_filtered_exploration_explo.GES_divided_by__SREF.sum(),2)
-                    transform_and_plot_stacked_bar_lot_et_slot(sous_lot_filtered_exploration_explo, 'Donnée environnementale (DE)', ['GES_divided_by__SREF'], [y_axis_min_sous_lots, y_axis_sous_lots])
+                #     #Making the stacked barplot from the choices
+                #     sous_lot_filtered_exploration_explo = group_and_calculate_percentage(sous_lot_filtered_exploration_explo, 'Donnée environnementale (DE)', 'GES_divided_by__SREF')
+                #     max_value = round(sous_lot_filtered_exploration_explo.GES_divided_by__SREF.sum(),2)
+                #     transform_and_plot_stacked_bar_lot_et_slot(sous_lot_filtered_exploration_explo, 'Donnée environnementale (DE)', ['GES_divided_by__SREF'], [y_axis_min_sous_lots, y_axis_sous_lots])
 
 
         else:
